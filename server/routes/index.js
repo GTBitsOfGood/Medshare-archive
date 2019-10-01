404af831a1f7ef80751706e2682a852c6fec5fd3
Sammy Baek <kie827@gmail.com>
2019-10-01 18:23:09 -0400

    add api endpoint for search

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = mongoose.connection.model('Product', {}, 'Product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("this is an API route");
+  res.send("this is the home API route");
+});
+
+/* GET search from query */
+router.get('/search', (req, res, next) => {
+  const q = req.query.q;
+  const query_fuzzy = {$regex: new RegExp(q, 'i')};
+  Product.find().or([{ProductName: query_fuzzy}, {'Category Name': query_fuzzy}, {'Sub Category': query_fuzzy}])
+      .then(products => {
+        return res.send(products);
+      })
+      .catch(err => {
+        return next(err);
+      })
 });
 
 module.exports = router;
