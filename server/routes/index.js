9370b6b5e7ba0b74151c03c43c68385bf22c558e
Sammy Baek <kie827@gmail.com>
2019-10-02 13:48:18 -0400

    general search checks ProductRef

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,7 +15,7 @@ router.get('/search', async (req, res, next) => {
   const query_fuzzy = {$regex: new RegExp(q, 'i')};
   let products = null;
   try {
-    products = await Product.find().or([{ProductName: query_fuzzy}, {'Category Name': query_fuzzy}, {'Sub Category': query_fuzzy}]);
+    products = await Product.find().or([{ProductName: query_fuzzy}, {'Category Name': query_fuzzy}, {'Sub Category': query_fuzzy}, {ProductRef: query_fuzzy}]);
   } catch (err) {
     return res.status(500).send(err);
   }
